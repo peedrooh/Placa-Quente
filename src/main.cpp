a6f97d15ebad8dd60eaa2e8c2334455d670ce72e
Pedro Henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2023-04-13 12:10:05 -0300

    Feat: statirng rotary switch implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-#include "assets/icons/icons.h"
+#include "models/menu/menu.h"
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -10,80 +10,134 @@
 #include <Wire.h>
 #endif
 
+#define RS_SWITCH_PIN 6
+#define RS_DT_PIN 7
+#define RS_CLK_PIN 8
+// Variables to debounce Rotary Encoder
+long RS_last_debounce_time = 0;
+int RS_debounce_delay = 5;
+int RS_increment = 0;
+bool turnDetect = false;
+bool prev_turnDetect = false;
 
-#define TEXT_LEFT_PADDING 29 // 2*ICON_LEFT_PADDING + ICON_WIDTH + 5
-#define TEXT_TOP_PADDING_TOP 4
+byte RS_PDT; 
+byte RS_DT; 
+byte RS_PCLK;
+byte RS_CLK;
+byte RS_is_clockwise;
 
-#define TEXT_HEIGHT 10
-
-#define ICON_LEFT_PADDING 4
-#define ICON_TOP_PADDING 2
-#define ICON_HEIGHT 16
-#define ICON_WIDTH 16
-
+int counter = 0;
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 
-void draw_srcrollbar() {
-    // Draw scrollbar scroll
-    for(int i = 1; i < 64; i++) {
-      u8g2.drawPixel(126, i);
-      i++;
+Menu* menu = new Menu();
+
+
+void check_RS_states() {
+    RS_CLK = digitalRead(RS_CLK_PIN);
+    RS_DT = digitalRead(RS_DT_PIN);
+
+    // Serial.print("----------- ");
+    // Serial.print(counter);
+    // Serial.println(" -----------");
+    // Serial.println(" PDT | PCLK | INC | DT | CLK");
+    // Serial.print("  ");
+    // Serial.print(RS_PDT);
+    // Serial.print("  |  ");
+    // Serial.print(RS_PCLK);
+    // Serial.print("   |  ");
+    // Serial.print(RS_increment);
+    // Serial.print("  |  ");
+    // Serial.print(RS_DT);
+    // Serial.print(" |  ");
+    // Serial.println(RS_CLK);
+    // Serial.println("");
+
+    if(RS_increment != 1) {
+        RS_increment = 0;
+        return;
     }
-    // draw scrollbar bar
-    u8g2.drawBox(125, 64/5 * 2, 3, 64/5);
-}
 
-void draw_focus_item() {
-    // Draw Foucus Item
-    u8g2.drawLine(1, 24, 1, 40);
-    u8g2.drawLine(1, 24, 3, 22);
-    u8g2.drawLine(3, 22, 119, 22);
-    u8g2.drawLine(119, 22, 121, 24);
-    u8g2.drawLine(121, 24, 121, 40);
-    u8g2.drawLine(120, 24, 120, 40);
-    u8g2.drawLine(121, 40, 119, 42);
-    u8g2.drawLine(119, 42, 3, 42);
-    u8g2.drawLine(3, 41, 119, 41);
-    u8g2.drawLine(3, 42, 1, 40);
+    if(RS_PDT != RS_PCLK && RS_DT != RS_CLK) {
+        RS_is_clockwise = true;
+        counter++;
+        RS_increment = 0;
+        return;
+    }
+
+    if(RS_DT != RS_CLK) {
+        RS_is_clockwise = true;
+        counter++;
+        RS_increment = 0;
+        return;
+    }
+
+    if(RS_PDT == RS_PCLK && RS_DT == RS_CLK && RS_PDT != RS_CLK) {
+        RS_is_clockwise = false;
+        counter--;
+        RS_increment = 0;
+        return;
+    } else {
+        counter++;
+    }
+
+    RS_increment = 0;
 };
 
-void text_prepare(const uint8_t * font, int drawColorState) {
-  u8g2.setFont(font);
-  u8g2.setFontRefHeightExtendedText();
-  u8g2.setDrawColor(drawColorState);
-  u8g2.setFontPosTop();
-  u8g2.setFontDirection(0);
+void RS_turn_detect() {
+    turnDetect = digitalRead(RS_CLK_PIN);
+    // Serial.println(turnDetect);
+
+    if(turnDetect != prev_turnDetect) {
+      prev_turnDetect = turnDetect;
+      delay(RS_debounce_delay/3);
+      RS_increment++;
+      if(((millis() - RS_last_debounce_time) > RS_debounce_delay)) {
+          RS_PDT = RS_DT;
+          RS_PCLK = RS_CLK;
+
+          check_RS_states();
+
+          RS_last_debounce_time = millis();
+      }
+    //   Serial.println(counter);'
+    }
 }
-
-void draw_menu() {
-  draw_srcrollbar();
-  draw_focus_item();
-
-  text_prepare(u8g2_font_haxrcorp4089_tr, 1);
-  u8g2.drawStr(TEXT_LEFT_PADDING, (TEXT_TOP_PADDING_TOP) , "Menu Principal");
-  u8g2.setDrawColor(1);
-  u8g2.drawXBMP(ICON_LEFT_PADDING, ICON_TOP_PADDING, ICON_WIDTH, ICON_HEIGHT, all_icons[5]->get_icon());
-
-  text_prepare(u8g2_font_helvB08_tr, 1);
-  u8g2.drawStr(TEXT_LEFT_PADDING, 22+(TEXT_TOP_PADDING_TOP) , "Temp. Constante");
-  u8g2.setDrawColor(1);
-  u8g2.drawXBMP(ICON_LEFT_PADDING, 22+ICON_TOP_PADDING, ICON_WIDTH, ICON_HEIGHT, all_icons[8]->get_icon());
-
-  text_prepare(u8g2_font_haxrcorp4089_tr, 1);
-  u8g2.drawStr(TEXT_LEFT_PADDING, 44+(TEXT_TOP_PADDING_TOP) , "Temporizador");
-  u8g2.setDrawColor(1);
-  u8g2.drawXBMP(ICON_LEFT_PADDING, 44+ICON_TOP_PADDING, ICON_WIDTH, ICON_HEIGHT, all_icons[7]->get_icon());
-};
 
 void setup(void) {
-  u8g2.begin();
-  // Serial.begin(9600);
+    u8g2.begin();
+    // Serial.begin(9600);
+    // Put current pins state in variables
+    pinMode(RS_DT_PIN, INPUT);
+    pinMode(RS_CLK_PIN, INPUT);
+
+    // // Set the Switch pin to use Arduino PULLUP resistors
+    // pinMode(RS_SWITCH_PIN, INPUT_PULLUP);
 }
 
 void loop(void) {
-  u8g2.clearBuffer();
-  draw_menu();
-  u8g2.sendBuffer();
-}
+
+    RS_turn_detect();
+
+    if(counter >= 6) counter--;
+    if(counter <= 0) counter++;
+    u8g2.clearBuffer();
+    menu->draw(u8g2, counter);
+    u8g2.sendBuffer();
+
+
+    // for(int i = 0; i < 6; i++) {
+    //     u8g2.clearBuffer();
+    //     menu->draw(u8g2, i);
+    //     u8g2.sendBuffer();
+    //     delay(500);
+    // }
+
+    // for(int i = 5; i > 0; i--) {
+    //     u8g2.clearBuffer();
+    //     menu->draw(u8g2, i);
+    //     u8g2.sendBuffer();
+    //     delay(500);
+    // }
+};
